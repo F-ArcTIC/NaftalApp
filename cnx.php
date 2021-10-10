8732d90dcb9d08d448589e8ff610c889d1c0da88
F-ArcTIC <88293932+F-ArcTIC@users.noreply.github.com>
2021-10-10 23:14:40 +0100

    Update cnx.php

--- cnx.php
+++ cnx.php
@@ -2,7 +2,6 @@
 try {
 	$conn = mysqli_connect("sql11.freemysqlhosting.net", "sql11443513", "PMbAtJmp4R", "sql11443513");
 	$conn->query("SET NAMES 'utf8'");
-}
 }catch (Exception $e){
 		
 		die('Erreur : impossible de se connecter à la base de donnée');
